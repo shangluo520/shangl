ef1ec01ce9df85d09a25b6db59c03684b107e899
shangluo520 <2225820780@qq.com>
2020-03-30 10:52:51 +0800

    修改

---
+++ Test1/src/Tools/StringTools.java
@@ -0,0 +1,20 @@
+package Tools;
+
+
+/*
+ * 	String¹¤¾ßÀà
+ * @author	WangWei
+ */
+public class StringTools {
+	
+	
+	/*
+	 * É¾³ý×Ö´®
+	 * 
+	 */
+	public static String escapeStringOne() {
+		return null;
+		
+	}
+
+}
